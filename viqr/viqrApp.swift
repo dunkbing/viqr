da6bb1a03c6237f7a4249008a8e2e650bda94fbd
binh.buidang <dangbinh4869@gmail.com>
2025-03-13 12:29:07 +0700

    init

---
+++ viqr/viqrApp.swift
@@ -0,0 +1,17 @@
+//
+//  viqrApp.swift
+//  viqr
+//
+//  Created by Bùi Đặng Bình on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct viqrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
